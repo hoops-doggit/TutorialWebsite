74da5a37bc1460543e538d8ca04549b638923dc9
Jacob Grossman <invisablelazerdog@gmail.com>
2026-08-08 16:01:41 +1200

    Removed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://hoops-doggit.github.io',
-    base: '/TutorialWebsite'
+    site: 'https://hoops-doggit.github.io'
 });
